15542cc0d323413c7f9afa20bbd2c99dd75ed3b7
Adrian Sutton <suttona@lmax.com>
2016-05-21 15:20:48 +1000

    Move test to asserting the result of the script instead of doing a simple text comparison.

--- src/test/java/net/symphonious/commonjs/CommonJsCompilerTest.java
+++ src/test/java/net/symphonious/commonjs/CommonJsCompilerTest.java
@@ -2,12 +2,16 @@ package net.symphonious.commonjs;
 
 import org.junit.Test;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CommonJsCompilerTest
 {
-    private static final String SIMPLE_JAVASCRIPT = "console.log('Hello World!');";
+    private static final String SIMPLE_JAVASCRIPT = "'Hello world!';";
 
     private final InMemoryModuleLoader moduleLoader = new InMemoryModuleLoader();
     private final CommonJsCompiler compiler = new CommonJsCompiler(moduleLoader);
@@ -17,6 +21,13 @@ public class CommonJsCompilerTest
     {
         moduleLoader.addModule("main", SIMPLE_JAVASCRIPT);
 
-        assertThat(compiler.compile("main"), is(SIMPLE_JAVASCRIPT));
+        assertScriptProducesOutput(compiler.compile("main"), "Hello world!");
+    }
+
+    private void assertScriptProducesOutput(final String script, final String expectedOutput) throws ScriptException
+    {
+        final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        final ScriptEngine javascriptEngine = scriptEngineManager.getEngineByMimeType("text/javascript");
+        assertThat(javascriptEngine.eval(script), is(expectedOutput));
     }
 }
